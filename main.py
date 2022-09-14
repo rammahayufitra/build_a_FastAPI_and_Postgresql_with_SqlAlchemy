29939fc00eaf2d0f7fab7db1ad178a03d7564d9b
ramma.fitra <rammafitracyt@gmail.com>
2022-09-14 16:43:36 +0700

    [add]commit4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from http.client import HTTPException
 from pydoc import describe
+from urllib import response
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
@@ -26,9 +27,10 @@ def get_all_items():
     items = db.query(models.Item).all()
     return items
 
-@app.get('/item/{item_id}')
+@app.get('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
 def get_an_item(item_id:int):
-    pass 
+    item = db.query(models.Item).filter(models.Item.id==item_id).first()
+    return item
 
 @app.post('/items',response_model=Item,status_code=status.HTTP_201_CREATED)
 def create_an_item(item:Item):
@@ -49,34 +51,27 @@ def create_an_item(item:Item):
 
     return new_item
 
-@app.put('/item/{item_id}')
-def update_an_item(item_id:int):
-    pass 
+@app.put('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def update_an_item(item_id:int, item:Item):
+    item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
+    item_to_update.name = item.name 
+    item_to_update.price = item.price 
+    item_to_update.description = item.description 
+    item_to_update.on_offer=item.on_offer 
+
+    db.commit() 
+
+    return item_to_update
 
 @app.delete('/item/{item_id}')
 def delete_item(item_id:int):
-    pass 
+    item_to_delete=db.query(models.Item).filter(models.Item.id==item_id).first()
+    if item_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Resource Not Found")
 
-# @app.get('/')
-# def index():
-#     return {"message":"hello world"}
+    db.delete(item_to_delete)
+    db.commit()
 
-# @app.get('/greet/{name}')
-# def greet_name(name:str):
-#     return {"greeting":f"Hello {name}"}
-
-# @app.get('/greet')
-# def greet_optional_name(name:Optional[str]="user"):
-#     return {"message":f"Hello {name}"}
-
-# @app.put('/item')
-# def update_item(item:Item):
-#     return { 
-#             'id':item.id,
-#             'name': item.name,
-#             'description': item.description, 
-#             'price':item.price,
-#             'on_offer': item.on_offer
-#             }
+    return item_to_delete 
 
 
